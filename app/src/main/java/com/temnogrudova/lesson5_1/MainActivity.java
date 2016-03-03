16445589446f9462a3a248951dcb5d0766f158e2
Temnogrudova <etemnogrudova@gmail.com>
2016-03-03 07:18:37 +0200

    Lesson5_1
    Tel ran

---
+++ app/src/main/java/com/temnogrudova/lesson5_1/MainActivity.java
@@ -0,0 +1,78 @@
+package com.temnogrudova.lesson5_1;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etText;
+    Button btnSave, btnLoad;
+    Context context;
+    SharedPreferences sPref;
+
+    final String SAVED_TEXT = "saved_text";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        context = getApplicationContext();
+        etText = (EditText) findViewById(R.id.etText);
+
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sPref = getPreferences(MODE_PRIVATE);
+                SharedPreferences.Editor ed = sPref.edit();
+                ed.putString(SAVED_TEXT, etText.getText().toString());
+                ed.commit();
+                Toast.makeText(context, "Text saved", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnLoad = (Button) findViewById(R.id.btnLoad);
+        btnLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sPref = getPreferences(MODE_PRIVATE);
+                String savedText = sPref.getString(SAVED_TEXT, "");
+                etText.setText(savedText);
+                Toast.makeText(context, "Text loaded", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
